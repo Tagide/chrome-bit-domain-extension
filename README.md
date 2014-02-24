d6f64c22f0431860327c31c22f0f7ca6c4db87ac
Tagide <admin@dotbit.me>
2014-02-25 00:15:29 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+Chrome extension for accessing the .bit TLD
+===========================================
+
+Bit domains are managed by the peer-to-peer Namecoin network with no central authority. The system is still at an early stage of development but grows rapidly.
+
+The authoritative source for .bit domains is the Namecoin block chain. This means normal DNS servers can currently not be used and surfing .bit domains remains difficult.
+
+This extension was created to make it as easy as possible for people to surf .bit domains. It simply redirects any requests for .bit domains to the dotbit.me proxy servers.
---
+++ proxy.js
@@ -0,0 +1,34 @@
+chrome.webRequest.onBeforeRequest.addListener(function (details) {
+  var parser = document.createElement('a');
+  parser.href = details.url;
+  var tld = parser.hostname.slice(-3);
+    if (tld != 'bit') {
+       	return;
+    } else {
+      // alert('Getting request for '+details.url);
+      var xhr = new XMLHttpRequest();
+      var url = "http://app.dotbit.me/"+parser.hostname.replace(".bit", "");
+      xhr.open("GET", url, false);
+      xhr.onreadystatechange = function() {
+      if (xhr.readyState == 4) {
+	var bitip = xhr.responseText;
+	console.log('IP: '+bitip+' and server url '+url);
+	var config = {
+          mode: "pac_script",
+          pacScript: {
+          data: "function FindProxyForURL(url, host) {\n" +
+                "  if (dnsDomainIs(host, '"+parser.hostname+"'))\n" +
+                "    return 'PROXY "+bitip+":80';\n" +
+                "  return 'DIRECT';\n" +
+                "}"
+          }
+        };
+	console.log('Config is: '+JSON.stringify(config));
+	chrome.proxy.settings.set({value: config, scope: 'regular'},function() {});
+        }
+      }
+      xhr.send();
+   }
+}, {
+    urls: ['http://*/*']
+}, ['blocking']);
