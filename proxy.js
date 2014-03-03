4592b26fecf4c4cdfc6626d1746cf9ba8e84a0a9
Tagide <admin@dotbit.me>
2014-03-03 17:52:22 +0100

    Update proxy.js

--- proxy.js
+++ proxy.js
@@ -7,7 +7,7 @@ chrome.webRequest.onBeforeRequest.addListener(function (details) {
     } else {
       // alert('Getting request for '+details.url);
       var xhr = new XMLHttpRequest();
-      var url = "http://app.dotbit.me/"+parser.hostname.replace(".bit", "");
+      var url = "http://app.dotbit.me/"+parser.hostname;
       xhr.open("GET", url, false);
       xhr.onreadystatechange = function() {
       if (xhr.readyState == 4) {
